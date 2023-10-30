76aa7b85d6551ca47a2561cd20af586bebd89858
SavioHaistem <savio.dantas.5858@gmail.com>
2023-10-30 16:13:33 -0300

    matriz lesson 50%

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("columns: ");
+        int columns = scan.nextInt();
+
+        System.out.print("lines: ");
+        int lines = scan.nextInt();
+
+        Integer[][] matriz = new Integer[lines][columns];
+
+
+        for (int countLines = 0; countLines < matriz.length; countLines++) {
+            for (int countColumns = 0; countColumns < matriz[countLines].length; countColumns++) {
+                System.out.printf("type line: %d, column: %d: ", countLines, countColumns);
+                matriz[countLines][countColumns] = scan.nextInt();
+            }
+        }
+
+        for (int countLines = 0; countLines < matriz.length; countLines++) {
+            for (int countColumns = 0; countColumns < matriz[countLines].length; countColumns++) {
+                System.out.print(matriz[countLines][countColumns] + "  ");
+            }
+            System.out.printf("%n");
+        }
+
+        System.out.print("type the number for search: ");
+        int wantedNumber = scan.nextInt();
+
+        for (int countLines = 0; countLines < matriz.length; countLines++) {
+            for (int countColumns = 0; countColumns < matriz[countLines].length; countColumns++) {
+                if (matriz[countLines][countColumns].equals(wantedNumber)) {
+                    System.out.printf("Position: L: %d, C: %d %n", countLines, countColumns);
+                    if (matriz[countLines][(countColumns - 1)] >= 0) {
+                        System.out.println("left: " + matriz[countLines][countColumns - 1]);
+                    };
+                }
+            }
+        }
+    }
+}
